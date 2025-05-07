4ee55c40444690b545d234539b1c41b3d22d229f
devillers <davidevillers@gmail>
2025-05-07 10:35:33 +0200

    strapi

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,6 @@
-// ./config/plugins.ts
+
+
+  // ./config/plugins.ts
 
 export default ({ env }) => ({
     upload: {
@@ -8,18 +10,21 @@ export default ({ env }) => ({
           cloud_name:   env('CLOUDINARY_NAME'),
           api_key:      env('CLOUDINARY_KEY'),
           api_secret:   env('CLOUDINARY_SECRET'),
+          secure:       true,                  // ← force https URLs
+          resource_type: 'auto',               // ← handle all file types
         },
         actionOptions: {
           upload: {
-            folder: env('CLOUDINARY_FOLDER'),        // for direct file uploads
+            folder: env('CLOUDINARY_FOLDER'), // ← for file uploads
           },
           uploadStream: {
-            folder: env('CLOUDINARY_FOLDER'),        // for internal streams (e.g. GraphQL, remote fetch)
+            folder: env('CLOUDINARY_FOLDER'), // ← for internal streams
           },
-          delete: {},       // you can also scope deletes if needed
+          delete: {},
           deleteMany: {},
         },
       },
     },
   });
+  
   
