876e17f51005feb23cc67ffe3d3866cc15df3f0b
devillers <davidevillers@gmail>
2025-05-07 09:19:34 +0200

    render

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,10 @@
-//server.ts
+// ./config/server.ts
 
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL'),                // ← required for correct public URL
+  proxy: env.bool('IS_PROXIED', true),   // ← important for correct HTTPS headers on Render
   app: {
     keys: env.array('APP_KEYS'),
   },
