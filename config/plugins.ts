45adcd7589ee0f49a280b1fab92def9fb4308856
devillers <davidevillers@gmail>
2025-05-07 10:16:41 +0200

    commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,12 +1,23 @@
+// ./config/plugins.ts
+
 export default ({ env }) => ({
     upload: {
       config: {
         provider: 'cloudinary',
         providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-          folder: env('CLOUDINARY_FOLDER', 'uploads'), // fallback if not set
+          cloud_name:   env('CLOUDINARY_NAME'),
+          api_key:      env('CLOUDINARY_KEY'),
+          api_secret:   env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {
+            folder: env('CLOUDINARY_FOLDER'),        // for direct file uploads
+          },
+          uploadStream: {
+            folder: env('CLOUDINARY_FOLDER'),        // for internal streams (e.g. GraphQL, remote fetch)
+          },
+          delete: {},       // you can also scope deletes if needed
+          deleteMany: {},
         },
       },
     },
