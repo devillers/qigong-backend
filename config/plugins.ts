bfc26e9a1e7b78de103d7d306261221b514019b9
devillers <davidevillers@gmail>
2025-05-07 08:33:16 +0200

    stappi

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+          folder: env('CLOUDINARY_FOLDER', 'uploads'), // fallback if not set
+        },
+      },
+    },
+  });
+  
--- config/server.ts
+++ config/server.ts
@@ -1,3 +1,5 @@
+//server.ts
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
